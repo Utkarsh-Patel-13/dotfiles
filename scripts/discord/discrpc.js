e2516b11afdfdb018a67eff197e53413de5ee70a
Utkarsh Patel <utkarshhpatel13@gmail.com>
2021-04-29 20:02:18 +0530

    discord changes

--- scripts/discord/discrpc.js
+++ scripts/discord/discrpc.js
@@ -1,48 +1,21 @@
-// const { error, log } = require("console");
 const RPC = require("discord-rpc"),
-  log = require("fancy-log");
+    log = require("fancy-log");
 const scopes = ["rpc", "rpc.api"];
 const rpc = new RPC.Client({
-  transport: "ipc",
+    transport: "ipc",
 });
 
 rpc.on("ready", () => {
-  rpc.setActivity({
-    // details: "",
-    state: "idk",
-    startTimestamp: new Date(),
-    largeImageKey: "formulacoin-_fc__02",
-    largeImageText: "FC",
-  });
+    rpc.setActivity({
+        state: "idk",
+        startTimestamp: new Date(),
+        largeImageKey: "formulacoin-_fc__02",
+        largeImageText: "FC",
+    });
 
-  console.log("RPC Loaded");
+    console.log("RPC Loaded");
 });
 
-rpc
-  .login({
-    clientId: "798402549437693982",
-    // scopes: scopes,
-  })
-  .catch(log.error);
-
-// const clientId = "798402549437693982";
-// const scopes = ["rpc", "rpc.api", "messages.read"];
-
-// const client = new RPC.Client({ transport: "websocket" });
-
-// client.on(
-//   "ready",
-//   () => {
-//     console.log("Logged in as", client.application.name);
-//     console.log("Authed for user", client.user.username);
-
-//     // client.selectVoiceChannel("81384788862181376");
-//   },
-//   "catch",
-//   () => {
-//     console.log("error");
-//   }
-// );
-
-// // Log in to RPC with client id
-// client.login({ clientId, scopes });
+rpc.login({
+    clientId: "<id>",
+}).catch(log.error);
